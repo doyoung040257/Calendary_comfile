f9a203dfe367a534e019c337428baf3f9b4480c5
Parksunho5514 <sunho5514@naver.com>
2025-09-04 15:11:16 +0900

    Update PersonalInfoPage.java

--- test/src/Settings/PersonalInfoPage.java
+++ test/src/Settings/PersonalInfoPage.java
@@ -16,83 +16,11 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-import frame.CalendarFrame;
+import frame.CalendarFrame01;
 import Settings.*;
 import lg.*;
 
 public class PersonalInfoPage extends JFrame {
-
-//	private JPanel panel;
-//	private Map<String, JTextField> textFields = new HashMap<>();
-//	private JButton savebtn;
-//	private JButton backButton;
-//
-//	public PersonalInfoPage() {
-//		setTitle("개인 정보");
-//
-//		panel = new JPanel();
-//		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-//
-//		// User user = UserSession.getUser(); // 로그인한 사용자 정보 가져오기
-//
-//		/*
-//		 * if (user == null) { JOptionPane.showMessageDialog(this, "로그인 정보가 없습니다.");
-//		 * FrameBase.getInstance(new FrameBegin()); dispose(); return; }
-//		 */
-//
-//		// 이름
-//		panel.add(new JLabel("이름:"));
-//		// JTextField nameField = new JTextField(user.getName());
-//		// textFields.put("name", nameField);
-//		// panel.add(nameField);
-//
-//		// 이메일
-//		panel.add(new JLabel("이메일:"));
-//		// JTextField emailField = new JTextField(user.getEmail());
-//		// textFields.put("email", emailField);
-//		// panel.add(emailField);
-//
-//		// 버튼
-//		savebtn = new JButton(LanguageManager.get("button.Save"));
-//		backButton = new JButton(LanguageManager.get("button.back"));
-//
-//		savebtn.addActionListener(e -> {
-//			String newName = textFields.get("name").getText();
-//			String newEmail = textFields.get("email").getText();
-//
-//			if (newName.isEmpty() || newEmail.isEmpty()) {
-//				JOptionPane.showMessageDialog(this, "모든 항목을 입력해주세요.");
-//				dispose();
-//				return;
-//			}
-//			// 세션의 user 객체 정보 업데이트
-//			// user.setName(newName);
-//			// user.setEmail(newEmail);
-//
-//			JOptionPane.showMessageDialog(this, "정보가 성공적으로 저장되었습니다.");
-//		});
-//
-//		// 뒤로가기 버튼
-//		backButton = new JButton(LanguageManager.get("button.back"));
-//		backButton.addActionListener(e -> {
-//			new SettingsMenu().setVisible(true);
-//			this.dispose(); // 현재 창 닫기
-//		});
-//
-//		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10)); // 가운데 정렬, 버튼 사이 간격 20px
-//		buttonPanel.add(savebtn);
-//		buttonPanel.add(backButton);
-//
-//		panel.add(buttonPanel);
-//
-//		add(panel);
-//		applyTheme();
-//
-//		setSize(400, 300);
-//		setVisible(true);
-//
-//	}
-
 
 	private User user;
 	private JPanel panel;
@@ -170,3 +98,4 @@ public class PersonalInfoPage extends JFrame {
 	}
 
 }
+
