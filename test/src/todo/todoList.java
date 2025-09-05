340cc3581ab7770609da0f920b09d4011b88fefe
doyoung040257 <doyoung040257@gmail.com>
2025-09-05 15:14:06 +0900

    Update todoList.java

--- test/src/todo/todoList.java
+++ test/src/todo/todoList.java
@@ -1,6 +1,5 @@
 package todo;
 
-
 public class todoList{
 
 	private String work;
@@ -14,55 +13,28 @@ public class todoList{
     	this.time = time;
     	this.note = note;
     }
-
-
-
 	public void setWork(String work) {
 		this.work = work;
 	}
-
 	public void setDay(String day) {
 		this.day = day;
 	}
-
 	public void setTime(String time) {
 		this.time = time;
 	}
-
 	public void setNote(String note) {
 		this.note = note;
 	}
-
 	public String getWork() {
 		return work;
 	}
-	
 	public String getDay() {
 		return day;
 	}
-
 	public String getTime() {
 		return time;
 	}
-
 	public String getNote() {
 		return note;
 	}
-	
-    @Override
-    public String toString() {
-        return "[할 일: " + work +
-               ", 날짜: " + day +
-               ", 시간: " + time +
-               ", 메모: " + note + "]";
-    }
-
-    
-    
 }
-
-
-
-//할 일 추가 완료 후 리스트 부분에 할일 추가
-//특정 갯수가 넘어가면 스크롤바 생김
-//리스트에 추가된 할 일 누르면 수정창 표시
