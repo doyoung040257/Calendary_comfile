75ea0acaeba611cf0003e8161196653de70af2a7
KJY-1204 <dbvcxz@gmail.com>
2025-09-08 15:05:56 +0900

    Update LoginFrame.java

--- test/src/lg/LoginFrame.java
+++ test/src/lg/LoginFrame.java
@@ -85,8 +85,8 @@ public class LoginFrame extends JFrame {
             User user = UserDatabase.userDatabase.get(id);
             if (user.getPassword().equals(pw)) {
                 JOptionPane.showMessageDialog(this, user.getName() + "님 환영합니다!");
-                CalendarFrame01 calendarFrame01 = new CalendarFrame01();
-                calendarFrame01.setVisible(true);
+                CalendarFrame01 appFrame = new CalendarFrame01(user); // 수정: user 객체 전달
+                appFrame.setVisible(true);
             } else {
                 JOptionPane.showMessageDialog(this, "비밀번호가 틀렸습니다.");
             }
@@ -95,5 +95,6 @@ public class LoginFrame extends JFrame {
         }
     }
 }
+
 
 
