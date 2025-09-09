9ee611bac992f79ada37d8532c64e175cac85873
Parksunho5514 <sunho5514@naver.com>
2025-09-09 17:08:04 +0900

    Update ThemeManager.java

--- test/src/Settings/ThemeManager.java
+++ test/src/Settings/ThemeManager.java
@@ -1,31 +1,50 @@
 package Settings;
 
 import java.awt.*;
-import java.awt.Container;
+import javax.swing.*;
 
 public class ThemeManager {
 
 	public static void applyTheme(Component component) {
-		if ("DEFAULT".equals(Setting.theme)) {
-			applyDefaultTheme(component);
-			return;
+		Color bgColor;
+		Color fgColor;
+
+		switch (Setting.theme) {
+		case "DARK":
+			bgColor = Color.DARK_GRAY;
+			fgColor = Color.WHITE;
+			break;
+		case "PASTEL":
+			bgColor = new Color(255, 228, 225);
+			fgColor = Color.BLACK;
+			break;
+		case "DEFAULT":
+		default:
+			bgColor = Color.decode("#D8BFD8");
+			fgColor = Color.BLACK;
+			break;
 		}
 
-		Color bgColor = Setting.theme.equals("DARK") ? Color.DARK_GRAY : Color.WHITE;
-		Color fgColor = Setting.theme.equals("DARK") ? Color.WHITE : Color.BLACK;
-
+		// 재귀적으로 테마 적용 (버튼 제외)
 		applyThemeRecursive(component, bgColor, fgColor);
 	}
 
 	private static void applyThemeRecursive(Component component, Color bg, Color fg) {
-		component.setBackground(bg);
-		component.setForeground(fg);
+	    if (component == null) return;
 
-		if (component instanceof Container) {
-			for (Component child : ((Container) component).getComponents()) {
-				applyThemeRecursive(child, bg, fg);
-			}
-		}
+	    // 버튼과 제외 표시된 컴포넌트는 건너뛰기
+	    if (!(component instanceof JButton) &&
+	        !(component instanceof JComponent && Boolean.TRUE.equals(
+	                ((JComponent) component).getClientProperty("excludeTheme")))) {
+	        component.setBackground(bg);
+	        component.setForeground(fg);
+	    }
+
+	    if (component instanceof Container) {
+	        for (Component child : ((Container) component).getComponents()) {
+	            applyThemeRecursive(child, bg, fg);
+	        }
+	    }
 	}
 
 	private static void applyDefaultTheme(Component component) {
@@ -40,18 +59,17 @@ public class ThemeManager {
 	}
 
 	public static void applyFontSettingPage(Container container, Font font) {
-	    applyFontRecursive(container, font);
-	    container.revalidate();
-	    container.repaint();
+		applyFontRecursive(container, font);
+		container.revalidate();
+		container.repaint();
 	}
 
 	private static void applyFontRecursive(Component comp, Font font) {
-	    comp.setFont(font);
-	    if (comp instanceof Container) {
-	        for (Component child : ((Container) comp).getComponents()) {
-	            applyFontRecursive(child, font);
-	        }
-	    }
+		comp.setFont(font);
+		if (comp instanceof Container) {
+			for (Component child : ((Container) comp).getComponents()) {
+				applyFontRecursive(child, font);
+			}
+		}
 	}
-
 }
