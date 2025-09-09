45a4b0181997305a340a7342a51440946af2f1fb
doyoung040257 <doyoung040257@gmail.com>
2025-09-09 15:35:15 +0900

    Update todoList.java

--- test/src/todo/todoList.java
+++ test/src/todo/todoList.java
@@ -1,6 +1,10 @@
 package todo;
 
-public class todoList{
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public class todoList implements Serializable{
 
 	private String work;
     private String day;
@@ -8,12 +12,15 @@ public class todoList{
     private String note;
     private int importance;
     
+    private final String id;
+    
     public todoList(String work, String day, String time, String note, int importance) {
     	this.work = work;
     	this.day = day;
     	this.time = time;
     	this.note = note;
     	this.importance	= importance;
+    	this.id = UUID.randomUUID().toString(); // 생성자마다 고유 ID 부여
     }
 	public void setWork(String work) {
 		this.work = work;
@@ -44,5 +51,21 @@ public class todoList{
 	}
 	public int getImportance() {
 		return importance;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj) return true;
+	    if (!(obj instanceof todoList)) return false;
+	    todoList other = (todoList) obj;
+	    return this.id.equals(other.id);
+	}
+
+	@Override
+	public int hashCode() {
+	    return Objects.hash(id);
 	}
 }
