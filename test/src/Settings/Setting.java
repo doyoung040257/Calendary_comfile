c5be942d03e2480690156a02110b5219b329bf84
Parksunho5514 <sunho5514@naver.com>
2025-09-09 15:35:46 +0900

    Update and rename Settings.java to Setting.java

--- test/src/Settings/Setting.java
+++ test/src/Settings/Setting.java
@@ -1,10 +1,11 @@
 package Settings;
 
-public class Settings {
+public class Setting {
 	
 	 public static String theme = "LIGHT"; // 기본 밝은 모드
 	 public static boolean notificationEnabled = true; // 기본 알림 설정 켜짐
 	 public static String language = "KR"; // "KR" = 한글, "EN" = 영어
+	 public static boolean notificationsEnabled;
 	 
 
 }
