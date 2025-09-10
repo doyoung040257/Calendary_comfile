17074dec2ae73309bdb72290e07d46ed05b9762e
KJY-1204 <dbvcxz@gmail.com>
2025-09-10 10:49:49 +0900

    Update NotificationManager.java

--- test/src/Settings/NotificationManager.java
+++ test/src/Settings/NotificationManager.java
@@ -7,7 +7,7 @@ public class NotificationManager {
 
     // 알림 출력 메서드
 	 public static void showNotification(String message) {
-	        if (Settings.notificationEnabled) {
+	        if (Setting.notificationEnabled) {
 	            JOptionPane.showMessageDialog(null, message);
 	        } else {
 	            // 알림 꺼짐 상태이므로 팝업 띄우지 않음
@@ -15,3 +15,4 @@ public class NotificationManager {
 	        }
 	    }
 }
+
