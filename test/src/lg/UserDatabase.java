aad223c7d6eb1a650ebafc2ea6497ea1e279429f
doyoung040257 <doyoung040257@gmail.com>
2025-09-10 09:02:49 +0900

    Update UserDatabase.java

--- test/src/lg/UserDatabase.java
+++ test/src/lg/UserDatabase.java
@@ -1,16 +1,31 @@
 package lg;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import todo.todoList;
+
 public class UserDatabase {
     public static Map<String, User> userDatabase = new HashMap<>();
+    public static Map<String, ArrayList<todoList>> userDatabase2 = new HashMap<>();
     private static final String FILE_NAME = "users.dat";
 
+    public static void addUser(User user) {
+        userDatabase.put(user.getId(), user);
+        saveUsers();
+    }
+
+    public static User getUser(String id) {
+        loadUsers(); // 최신 데이터 불러오기
+        return userDatabase.get(id);
+    }
+    
     public static void saveUsers() {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
             oos.writeObject(userDatabase);
+            oos.writeObject(userDatabase2);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -20,8 +35,10 @@ public class UserDatabase {
     public static void loadUsers() {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             userDatabase = (Map<String, User>) ois.readObject();
+            userDatabase2 = (Map<String, ArrayList<todoList>>) ois.readObject();
         } catch (Exception e) {
             userDatabase = new HashMap<>();
+            userDatabase2 = new HashMap<>();
         }
     }
 }
