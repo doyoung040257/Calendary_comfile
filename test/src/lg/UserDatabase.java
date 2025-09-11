354e0b5885c098f086a064ff928fee5dff077af9
doyoung040257 <doyoung040257@gmail.com>
2025-09-11 16:53:53 +0900

    Update UserDatabase.java

--- test/src/lg/UserDatabase.java
+++ test/src/lg/UserDatabase.java
@@ -9,7 +9,6 @@ import todo.todoList;
 
 public class UserDatabase {
     public static Map<String, User> userDatabase = new HashMap<>();
-    public static Map<String, ArrayList<todoList>> userDatabase2 = new HashMap<>();
     private static final String FILE_NAME = "users.dat";
 
     public static void addUser(User user) {
@@ -25,7 +24,6 @@ public class UserDatabase {
     public static void saveUsers() {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
             oos.writeObject(userDatabase);
-            oos.writeObject(userDatabase2);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,10 +33,9 @@ public class UserDatabase {
     public static void loadUsers() {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             userDatabase = (Map<String, User>) ois.readObject();
-            userDatabase2 = (Map<String, ArrayList<todoList>>) ois.readObject();
         } catch (Exception e) {
             userDatabase = new HashMap<>();
-            userDatabase2 = new HashMap<>();
         }
     }
 }
+
