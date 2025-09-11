57008f2b1b76ed46a5dbef19b309d54726db731c
doyoung040257 <doyoung040257@gmail.com>
2025-09-11 17:34:59 +0900

    Update MonthlyCalendarView.java

--- test/src/frame/MonthlyCalendarView.java
+++ test/src/frame/MonthlyCalendarView.java
@@ -155,7 +155,7 @@ public class MonthlyCalendarView extends JFrame {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e) {
                 if (isCurrentMonth) {
-                    TodoPageView todoPage = new TodoPageView(date, mainFrame, user);
+                    TodoPageView todoPage = new TodoPageView(date, mainFrame);
                     todoPage.setVisible(true);
                     dispose();
                 }
@@ -253,4 +253,5 @@ public class MonthlyCalendarView extends JFrame {
         return panel;
     }
 }
+
 
