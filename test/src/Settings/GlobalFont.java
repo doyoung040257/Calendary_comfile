a7d5cab3bb292430e050c2d53eca40298ae12a46
Parksunho5514 <sunho5514@naver.com>
2025-09-12 12:19:44 +0900

    Update GlobalFont.java

--- test/src/Settings/GlobalFont.java
+++ test/src/Settings/GlobalFont.java
@@ -1,9 +1,30 @@
 package Settings;
 
-import java.awt.Font;
+import java.awt.*;
+import java.awt.Component;
+import java.awt.Container;
+
+import javax.swing.JFrame;
 
 public class GlobalFont {
-	
-	public static Font currentFont = new Font("맑은 고딕", Font.PLAIN, 14); // 기본 글꼴
+    // 전역 글꼴 저장
+    public static Font currentFont = null;
 
+    // 재귀적으로 컨테이너에 폰트 적용
+    public static void applyFontRecursively(Container container, Font font) {
+        container.setFont(font);
+        for (Component comp : container.getComponents()) {
+            comp.setFont(font);
+            if (comp instanceof Container) {
+                applyFontRecursively((Container) comp, font);
+            }
+        }
+    }
+
+    // 편의 메서드: JFrame에 적용
+    public static void applyFont(JFrame frame) {
+        if (currentFont != null) {
+            applyFontRecursively(frame.getContentPane(), currentFont);
+        }
+    }
 }
