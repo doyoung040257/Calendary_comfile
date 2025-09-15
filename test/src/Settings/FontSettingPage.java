d30491a6ed90f7ff987a3ea82da4f35636d33c16
doyoung040257 <doyoung040257@gmail.com>
2025-09-15 10:07:53 +0900

    Update FontSettingPage.java

--- test/src/Settings/FontSettingPage.java
+++ test/src/Settings/FontSettingPage.java
@@ -143,5 +143,47 @@ public class FontSettingPage extends JFrame {
         }
         container.setFont(font);
     }
+
+        private JButton createNavButton(String text) {
+        JButton button = new JButton(text) {
+        
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // 배경 색상 (눌렸을 때 어둡게)
+            if (getModel().isArmed()) {
+                g2.setColor(getBackground().darker());
+            } else {
+                g2.setColor(getBackground());
+            }
+            // 둥근 사각형 배경
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), 30, 30);
+            g2.dispose();
+            // 버튼 텍스트 그대로 출력
+            super.paintComponent(g);
+        }
+
+        @Override
+        protected void paintBorder(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(Color.GRAY); // 테두리 색상
+            g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+            g2.dispose();
+        }
+    };
+	    button.setBackground(Color.WHITE);
+	    button.setForeground(Color.BLACK);
+	
+	    // 기본 버튼 효과 제거
+	    button.setContentAreaFilled(false);
+	    button.setFocusPainted(false);
+	    button.setBorderPainted(false);
+	    button.setOpaque(false);
+        
+        return button;
+    }
 }
+
 
